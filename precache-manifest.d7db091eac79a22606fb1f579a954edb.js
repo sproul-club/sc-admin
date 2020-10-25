157ff80f169207d22dd3ef146bc8d98a03cc1da1
Tejas Shah <tejashah88@gmail.com>
2020-10-25 10:18:14 -0700

    Updates

---
+++ precache-manifest.d7db091eac79a22606fb1f579a954edb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3ebcc602cea85146b26b1ece6d0591",
+    "url": "/sc-admin/index.html"
+  },
+  {
+    "revision": "7c9141541d541dfdae27",
+    "url": "/sc-admin/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ff707508852e145fe01c",
+    "url": "/sc-admin/static/css/main.3c5ec51f.chunk.css"
+  },
+  {
+    "revision": "7c9141541d541dfdae27",
+    "url": "/sc-admin/static/js/2.dba36f48.chunk.js"
+  },
+  {
+    "revision": "469f3e0caf61582287e7078bcfc8446d",
+    "url": "/sc-admin/static/js/2.dba36f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff707508852e145fe01c",
+    "url": "/sc-admin/static/js/main.34639a0c.chunk.js"
+  },
+  {
+    "revision": "4169a219474265efeed7",
+    "url": "/sc-admin/static/js/runtime-main.98c1562d.js"
+  },
+  {
+    "revision": "0f4508cd3bc1f76e0c4d9a396cdeff7f",
+    "url": "/sc-admin/static/media/logo.0f4508cd.png"
+  }
+]);
