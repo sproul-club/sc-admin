5ead5acd9ad4fdb629a07598809cc7ead14d3dd6
Tejas Shah <tejashah88@gmail.com>
2021-02-09 17:33:31 -0800

    added combined line chart for club history [WIP]

--- src/pages/OverviewPage.js
+++ src/pages/OverviewPage.js
@@ -15,7 +15,7 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRedo } from '@fortawesome/free-solid-svg-icons';
 
-import ChartComponent from 'react-chartjs-2';
+import ReactEcharts from 'echarts-for-react';
 
 import { GlobalAPI } from '../utils/backendClient';
 import DataLoaderPage from './DataLoaderPage';
@@ -24,53 +24,6 @@ const BASE_PADDING = 48;
 const SIDE_PADDING = 96;
 const SECTION_PADDING = 16;
 const CARD_GAP = 6;
-
-const DataChart = ({ type, title, data, dataLabels, dataDescription, bgColors, borderColors, showLegend = false, beginAtZero = false }) => {
-  const chartData = {
-    labels: dataLabels,
-    datasets: [{
-      label: dataDescription,
-      data: data,
-      fill: false,
-      backgroundColor: bgColors,
-      borderColor: borderColors,
-      borderWidth: 1
-    }]
-  };
-
-  const chartOptions = {
-    title: {
-      display: true,
-      text: title,
-      fontSize: 18
-    },
-    legend: {
-      display: showLegend
-    },
-    responsive: true,
-    maintainAspectRatio: true,
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: beginAtZero,
-            callback: function(value) { if (value % 1 === 0) { return value; } }
-          },
-        }
-      ],
-    },
-  };
-
-  return (
-    <ChartComponent
-      data={chartData}
-      options={chartOptions}
-      type={type}
-      height={50}
-      width={100}
-    />
-  );
-};
 
 const Card = props => (
   <Box px="4" py="5" rounded="sm" shadow="lg" {...props} />
@@ -142,6 +95,57 @@ const OverviewStatsDashboard = ({ stats, reload }) => {
   const studentsConfirmed = stats.sign_up.student.main.students_confirmed;
   const studentsConfirmedRecent = stats.sign_up.student.changed.students_confirmed;
 
+  const option = {
+    title: {
+      text: 'Club History'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data: ['Registered Clubs', 'Confirmed Clubs', 'Reactivated Clubs']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: [...Array(clubsRegisteredHistory.length).keys()].map(x => clubsRegisteredHistory.length - x - 1).map(x => `${x} weeks ago`),
+      axisLabel: {
+        rotate: 30
+      }
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: 'Registered Clubs',
+        type: 'line',
+        data: clubsRegisteredHistory
+      },
+      {
+        name: 'Confirmed Clubs',
+        type: 'line',
+        data: clubsConfirmedHistory
+      },
+      {
+        name: 'Reactivated Clubs',
+        type: 'line',
+        data: clubsReactivatedHistory
+      }
+    ]
+  };
+
   return (
     <Tabs
       isLazy
@@ -185,36 +189,9 @@ const OverviewStatsDashboard = ({ stats, reload }) => {
               />
             </Grid>
 
-            <Grid templateColumns="repeat(3, 1fr)" gap={CARD_GAP}>
+            <Grid templateColumns="repeat(1, 1fr)" gap={CARD_GAP}>
               <Card>
-                <DataChart
-                  type="bar"
-                  title="Registered clubs over time"
-                  dataLabels={[...Array(clubsRegisteredHistory.length).keys()].map(x => clubsRegisteredHistory.length - x - 1)}
-                  data={clubsRegisteredHistory}
-                  dataDescription="# of clubs"
-                  bgColors="rgba(255, 99, 132, 0.4)"
-                  beginAtZero />
-              </Card>
-              <Card>
-                <DataChart
-                  type="bar"
-                  title="Confirmed clubs over time"
-                  dataLabels={[...Array(clubsConfirmedHistory.length).keys()].map(x => clubsConfirmedHistory.length - x - 1)}
-                  data={clubsConfirmedHistory}
-                  dataDescription="# of clubs"
-                  bgColors="rgba(75, 192, 192, 0.4)"
-                  beginAtZero />
-              </Card>
-              <Card>
-                <DataChart
-                  type="bar"
-                  title="Reactivated clubs over time"
-                  dataLabels={[...Array(clubsReactivatedHistory.length).keys()].map(x => clubsReactivatedHistory.length - x - 1)}
-                  data={clubsReactivatedHistory}
-                  dataDescription="# of clubs"
-                  bgColors="rgba(54, 162, 235, 0.4)"
-                  beginAtZero />
+                <ReactEcharts option={option} />
               </Card>
             </Grid>
 
